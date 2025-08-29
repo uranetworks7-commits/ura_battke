f4c27dbc7fd2b09bdf4bfc296fdab8ac24479880
Utkarsh Singh <uranetworks6@gmail.com>
2025-08-29 06:34:26 +0000

    Not working please fix game Box on Portant mod of full screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex h-screen flex-col bg-background font-body" style={{ backgroundImage: `url('https://i.postimg.cc/y8ZBRDXQ/mmm.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <main className="h-screen w-screen flex flex-col bg-background font-body" style={{ backgroundImage: `url('https://i.postimg.cc/y8ZBRDXQ/mmm.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="flex-1 flex flex-col items-center justify-center text-center text-white bg-black/60 backdrop-blur-sm p-4 sm:p-8">
         {!gameConfig && !spectatorConfig ? (
           <JoinGameForm onStartGame={handleStartGame} onStartSpectating={handleStartSpectating} />
